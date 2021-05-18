8f02618cb959174e1347d53784c2e40446a8299d
Royal <thaparoyal17@gmail.com>
2021-05-18 20:49:47 +0545

    Login Page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Login from "./components/Login";
 
 function App() {
-  return <div className="app">Spotify Clone</div>;
+  return (
+    <div className="app">
+      <Login />
+    </div>
+  );
 }
 
 export default App;
