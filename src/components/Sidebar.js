c84390956a0f118591ae506a5e8f8255bcc6b694
Royal <thaparoyal17@gmail.com>
2021-05-19 17:25:22 +0545

    sidebar UI

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,6 +1,9 @@
 import React from "react";
 import "./Sidebar.css";
 import SidebarOptions from "./SidebarOptions";
+import HomeIcon from "@material-ui/icons/Home";
+import SearchIcon from "@material-ui/icons/Search";
+import LibraryMusicIcon from "@material-ui/icons/LibraryMusic";
 
 function Sidebar() {
   return (
@@ -10,9 +13,15 @@ function Sidebar() {
         src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
         alt=""
       />
-      <SidebarOptions title="Home" />
-      <SidebarOptions title="Search" />
-      <SidebarOptions title="Your Library" />
+      <SidebarOptions Icon={HomeIcon} title="Home" />
+      <SidebarOptions Icon={SearchIcon} title="Search" />
+      <SidebarOptions Icon={LibraryMusicIcon} title="Your Library" />
+      <br />
+      <strong className="sidebar__title">PLAYLISTS</strong>
+      <hr />
+      <SidebarOptions title="Hip Hop" />
+      <SidebarOptions title="Rock  " />
+      <SidebarOptions title="RnB  " />
     </div>
   );
 }
